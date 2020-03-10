0483234d39e4006e3762d1ff67b653563cb7c049
William Gomez <william.gomex@gmail.com>
2020-03-09 19:28:36 -0500

    feature/videos-bio: Video in all viewports.

--- components/BioTimeline/index.js
+++ components/BioTimeline/index.js
@@ -1,11 +1,13 @@
-const boxHeigth = 150
-const boxMargin = 50
-const cardSeparation = 100
+const boxHeigth = { sm: 200, md: 150 }
+const boxMargin = { sm: 20, md: 50 }
+const cardSeparation = { sm: 250, md: 100 }
+const ellipsisHeight = { sm: 50, md: 100 }
 
 const BioTimeline = {
   props: ['content'],
   data() {
     return {
+      mq: 'sm',
       cards: this.content.cards
     }
   },
@@ -17,25 +19,36 @@ const BioTimeline = {
     },
     getCardOffset(index) {
       return {
-        top: `${index * cardSeparation}px`
+        top: `${index * cardSeparation[this.mq]}px`
       }
+    },
+    setViewport(evt) {
+      if (!evt) {
+        evt = { matches: document.documentElement.clientWidth < 768 }
+      }
+      this.mq = evt.matches ? 'sm' : 'md'
     }
   },
   computed: {
     bioHeight() {
       return {
-        height: `${(this.cards.length - 1) * cardSeparation +
-          boxHeigth +
-          2 * boxMargin}px`
+        height: `${(this.cards.length - 1) * cardSeparation[this.mq] +
+          boxHeigth[this.mq] +
+          2 * boxMargin[this.mq]}px`
       }
     },
     bioSizes() {
       return {
-        '--box-height': `${boxHeigth}px`,
-        '--margin-box': `${boxMargin}px`,
-        '--card-separation': `${cardSeparation}`
+        '--box-height': `${boxHeigth[this.mq]}px`,
+        '--margin-box': `${boxMargin[this.mq]}px`,
+        '--ellipsis-height': `${ellipsisHeight[this.mq]}px`
       }
     }
+  },
+  mounted() {
+    this.setViewport()
+    const mql = window.matchMedia('(max-width: 768px)')
+    mql.addListener(this.setViewport)
   }
 }
 
